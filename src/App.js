663be3372c94c7359e9a30bf9cf9efa42aa34be7
pablomarcel <pablomarcel@outlook.com>
2022-05-31 19:56:43 -0700

    end of first video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React from "react";
+import Header from './components/Header'
+import './default.scss'
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
-
-      <h2>hello world</h2>
+        <Header/>
+        <div className="main">
+            <Homepage/>
+        </div>
 
     </div>
   );
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.scss'
+import Directory from "../../components/Directory";
+
+const Homepage = props =>{
+    return(
+        <section className="homepage">
+            <Directory/>
+
+        </section>
+    )
+}
+
+export default Homepage
